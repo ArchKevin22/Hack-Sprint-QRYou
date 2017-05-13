aea284b086b055966d2f92e80809a965535dfb76
Kevin Kou <kevin.kou@ucla.edu>
2017-05-13 10:16:23 -0700

    Doot doot

---
+++ qryou/QRCodeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  QRCodeViewController.swift
+//  qryou
+//
+//  Created by Kevin Kou on 5/10/17.
+//  Copyright © 2017 Team Null. All rights reserved.
+//
+
+import UIKit
+
+class QRCodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
